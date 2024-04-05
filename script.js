bf039f44a0869caa6477fb7708c1c10131e97a7b
acharya <sav.acharya22@gmail.com>
2024-04-06 10:36:47 +1100

    feat: put in function to stop code running

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-
-
-let pokemonTeam = [
-     "Bulbasaur",
-   "Ivysaur",
- "Charmander",
-
- 
-]
-
+let pokemonTeam = ["Bulbasaur", "Ivysaur", "Charmander", "Chewa"];
 
 function logTheTeam() {
-    sortedTeam = pokemonTeam.sort()
+  sortedTeam = pokemonTeam.sort();
 
-    sortedTeam.forEach((pokemon, arrayIndex) => {
-        console.log(pokemon + "is at index of " + arrayIndex );
-    });
+  sortedTeam.forEach((pokemon, arrayIndex) => {
+    console.log(pokemon + "is at index of " + arrayIndex);
+  });
 
-    // let [first, ...others] = sortedTeam
+  // let [first, ...others] = sortedTeam
 
-    // console.log(first)
-    // console.log(others)
+  // console.log(first)
+  // console.log(others)
 
-    console.log(pokemonTeam.entries());
+  console.log(pokemonTeam.entries());
 
-    for (const [index, pokemon] of sortedTeam.entries()) { 
-        console.log(`${pokemon} is at index of ${index}`)
-    }
-
-
+  for (const [index, pokemon] of sortedTeam.entries()) {
+    console.log(`${pokemon} is at index of ${index}`);
+  }
 }
 
-logTheTeam();
+// logTheTeam();
+
+const logTheTeamAlternate = (targetsize = 4) => {
+  if (pokemonTeam.length == targetsize) {
+    console.log("There are 4 pokemon in the team");
+  }
+
+  if (pokemonTeam.length === targetsize) {
+    console.log("There are 4 pokemon in the team, this is from strict");
+  }
+};
+
+logTheTeamAlternate();
+logTheTeamAlternate(2);
